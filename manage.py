d699e4437bba0a04c97fb1de12e6c7a8cc815fe9
joker <joker@jokermacpro.local>
2018-12-28 19:27:17 +0800

    配置文件修改

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import os
 
 from app import create_app, db
 
-
+from app.models import Author, Book
 
 from flask_script import Manager, Shell
 
@@ -19,7 +19,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, Author=Author, Book=Book)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
