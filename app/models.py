798954dee52c5be7553150fa7a2100fbcddd8aa8
joker <joker@jokermacpro.local>
2018-12-28 19:37:57 +0800

    配置文件

--- app/models.py
+++ app/models.py
@@ -4,20 +4,9 @@
 # @Date    : 2018-12-27
 from . import db
 
-class Author(db.Model):
-    __tablename__ = "authors"
+class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(32))
 
-    #关系属性
-    books = db.relationship("Book",backref="author")
-
-#书籍模型(多方)
-class Book(db.Model):
-    __tablename__ = "books"
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(32))
-
-    #外键
-    author_id = db.Column(db.Integer,db.ForeignKey(Author.id))
 
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import os
 
 from app import create_app, db
 
-
+from app.models import User
 
 from flask_script import Manager, Shell
 
@@ -19,11 +19,11 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, User=User)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
